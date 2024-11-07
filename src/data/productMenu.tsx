7d9b5d7af9dc568fc315b2a94891ed8a3abeb6c7
gideon <you@example.com>
2024-11-07 17:02:28 +0300

    deployment fix3

--- src/data/productMenu.tsx
+++ src/data/productMenu.tsx
@@ -1,27 +1,26 @@
-import InventoryIcon from '@mui/icons-material/Inventory';
-import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
-import TimeToLeaveIcon from '@mui/icons-material/TimeToLeave';
-import CoffeeMakerIcon from '@mui/icons-material/CoffeeMaker';
+import InventoryIcon from "@mui/icons-material/Inventory";
+import MedicalServicesIcon from "@mui/icons-material/MedicalServices";
+import TimeToLeaveIcon from "@mui/icons-material/TimeToLeave";
+
 const Menu = [
   {
     id: 1,
     type: "all",
     name: "All Products",
-    icon:  <InventoryIcon /> ,
+    icon: <InventoryIcon />,
   },
   {
     id: 2,
     type: "medical",
     name: "Medical",
-    icon:  <MedicalServicesIcon /> ,
+    icon: <MedicalServicesIcon />,
   },
   {
     id: 3,
     type: "automobile",
     name: "Automobile",
-    icon:  <TimeToLeaveIcon /> ,
+    icon: <TimeToLeaveIcon />,
   },
-  
 ];
 
 export default Menu;
